aeb3eac70a066f77cd98bc230e12b1da1611314c
Saba Nisar Shaikh <sabanisar990@gmail.com>
2024-08-31 18:35:45 +0500

    stress analyzer

---
+++ UniProject/UniProject/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def form(request):
+    return render(request, 'form.html')
